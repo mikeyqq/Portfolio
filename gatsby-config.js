ba3201870674db34b663e8bd88666a0ee7ed7e9e
Anchen <acmhuang@gmail.com>
2019-11-24 17:23:24 -0500

    adjusted page logo in gatsy config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
+    title: 'Anchen Huang',
     author: 'Hunter Chang',
     description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
   },
@@ -15,7 +15,7 @@ module.exports = {
         backgroundColor: '#663399',
         themeColor: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/page_logo.svg', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
